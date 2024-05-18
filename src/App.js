7347d1d40300cc1cc5ff4f34d5683968df146b24
Elanur Birinci <elanurrbirinci@gmail.com>
2024-05-19 02:45:55 +0300

    Datatable oluşturuldu

--- src/App.js
+++ src/App.js
@@ -1,23 +1,79 @@
-import logo from './logo.svg';
+
 import './App.css';
+import DataTable from 'react-data-table-component';
+import React, {useState} from 'react';
 
 function App() {
+
+  const columns=[
+    {
+      name: 'Avatar',
+      
+    },
+    {
+      name: 'Name',
+      selector: row =>row.name
+      
+    },
+    {
+      name: 'Userame',
+      selector: row =>row.username
+      
+    },
+    {
+      name: 'Email',
+      selector: row =>row.email
+    },
+    {
+      name: 'Role',
+      selector: row =>row.role
+    },
+    {
+      name:'Edit',
+      
+    },
+
+  ];
+
+  const data=[
+    {
+      id:1,
+      name:'Ela',
+      username:'Yılmaz',
+      email:'ela@gmail.com',
+      role:'Administrator'
+    },
+    {
+      id:2,
+      name:'Leyla',
+      username:'Aslan',
+      email:'leyla@gmail.com',
+      role:'Author'
+    }
+    
+  ]
+
+  const [records, setRecords]=useState(data);
+
+  function handleFilter(event){
+    const newData=data.filter(row =>{
+      return row.name.toLowerCase().includes(event.target.value.toLowerCase())
+    })
+    setRecords(newData)
+  }
+
   return (
+
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='text-end'><input type='text' onChange={handleFilter}/></div>
+      <DataTable
+      columns={columns}
+      data={records}
+      selectableRows
+      fixedHeader
+      pagination></DataTable>
+      
+      
     </div>
   );
 }
